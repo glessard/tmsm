ab3bd1ef98769a27c1b6cbd371ff351052b32e8f
Guillaume Lessard <dhtnstff@gmail.com>
2021-02-17 14:46:43 -0700

    improve launch function(s)

--- Sources/tmsm/launch.swift
+++ Sources/tmsm/launch.swift
@@ -5,33 +5,75 @@
 import Foundation
 import struct ArgumentParser.ExitCode
 
-@discardableResult
 func launch(command: String, arguments: String...,
             environment: [String: String] = [:]) throws -> String
 {
-  let task = Process()
-  task.launchPath = "/usr/bin/env"
-
   let pipe = Pipe()
-  task.standardOutput = pipe
-  task.standardError = FileHandle.standardError
+  do {
+    try launch(command: command,
+               arguments: arguments,
+               environment: environment,
+               output: pipe.fileHandleForWriting)
+  }
+  catch {
+    let data = pipe.fileHandleForReading.availableData
+    FileHandle.standardOutput.write(data)
+    throw error
+  }
+
+  let data = pipe.fileHandleForReading.availableData
+  return String(data: data, encoding: .utf8) ?? ""
+}
+
+func launch(command: String? = nil, arguments: [String], environment: [String: String] = [:],
+            output: FileHandle = .standardOutput, error: FileHandle = .standardError) throws
+{
+  guard let exec = command ?? arguments.first
+  else {
+    let message = "missing command in #function\n"
+    FileHandle.standardError.write(Data(message.utf8))
+    throw ExitCode(EX_UNAVAILABLE)
+  }
+
+  var dir = ObjCBool(false)
+  guard FileManager.default.fileExists(atPath: exec, isDirectory: &dir)
+  else {
+    let message = "Path \"\(exec)\" does not exist\n"
+    FileHandle.standardError.write(Data(message.utf8))
+    throw ExitCode(EX_UNAVAILABLE)
+  }
+
+  if dir.boolValue == true
+  {
+    let message = "Path \"\(exec)\" represents a directory\n"
+    FileHandle.standardError.write(Data(message.utf8))
+    throw ExitCode(EX_USAGE)
+  }
+
+  guard FileManager.default.isExecutableFile(atPath: exec)
+  else {
+    let message = "File \"\(exec)\" is not executable\n"
+    FileHandle.standardError.write(Data(message.utf8))
+    throw ExitCode(EX_NOPERM)
+  }
+
+  let task = Process()
+  task.launchPath = command ?? "/usr/bin/env"
+
+  task.standardOutput = output
+  task.standardError = error
   if !environment.isEmpty
   {
     let env = ProcessInfo.processInfo.environment
     task.environment = env.merging(environment, uniquingKeysWith: { $1 })
   }
 
-  task.arguments = [command] + arguments
+  task.arguments = arguments
   try task.run()
   task.waitUntilExit()
 
-  let data = pipe.fileHandleForReading.availableData
-  let output = String(data: data, encoding: .utf8) ?? ""
   if task.terminationStatus != 0
   {
-    FileHandle.standardOutput.write(Data(output.utf8))
     throw ExitCode(task.terminationStatus)
   }
-
-  return output
 }
